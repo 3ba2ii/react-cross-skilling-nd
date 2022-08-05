530bcbab75f0009a72c546924db36ba8c437a3d3
3ba2ii <aghonem2011@gmail.com>
2022-08-05 18:31:26 +0200

    feat: 🎸 added starter code for week 1

---
+++ week_1/starter/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className='App main-layout'>
+      <section className='side-navbar-section'>Logo</section>
+    </main>
+  );
+}
+
+export default App;
